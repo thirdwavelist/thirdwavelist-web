0f88bc564a29e5f320b0124ad0336cf7e7775f46
Antal János Monori <anthonymonori@gmail.com>
2018-02-27 23:24:40 +0100

    Updated depdendencies, updated meta viewport declaration, added new route to submission, added new navigation bar using reactstrap, fixed some js lint warnings

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from "react";
 import Routes from "./Routes";
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <h1 className="title"><a className="no-style" href="/" alt="Home">Third Wave List</a></h1>
+        <Navbar expand="md" light>
+          <NavbarBrand className="title" href="/" alt="Home">Third Wave List</NavbarBrand>
+          <Nav right navbar>
+            <NavItem>
+              <NavLink className="nav-item"  href="/submit" alt="Submit café">Submit</NavLink>
+            </NavItem>
+          </Nav>
+        </Navbar>
+      
         <Routes />
+
         <div className="footer">
           <span>Made with <span role="img" aria-label="love">❤️</span> by <a className="no-style" href="https://www.twitter.com/tjalve">@tjalve</a> and <a className="no-style" href="https://www.twitter.com/anthonymonori">@anthonymonori</a>.</span>
           <br />
---
+++ src/containers/Submit.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default() =>
+    <div>
+        <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"/>
+        <iframe title="Embedded submit form" class="airtable-embed airtable-dynamic-height" src="https://airtable.com/embed/shrLbdxmSZtDZW6uW?backgroundColor=cyan" frameborder="0" onmousewheel="" width="100%" height="3952" styles={{ background: 'transparent', border: '1px solid #ccc' }}/>
+    </div>
